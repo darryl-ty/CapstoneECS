8cd7fe08298f67f4266b40d1bf8ad4fc31959d40
darryl-ty <darrylterrell921@gmail.com>
2024-03-31 21:55:41 -0500

    Start implementation of  history recording of establishment of kingdoms for world.

--- src/systems.h
+++ src/systems.h
@@ -2,13 +2,14 @@
 #ifndef SYSTEMS_H
 #define SYSTEMS_H
 #include "entitymanager.h"
+#include "componentmanager.h"
+
+extern int START_YEAR;
 
 struct KingdomSystem{
-    static void update(EntityManager& entityManager){
-        for (auto& entity : ENTITY_SET){
-            if (entityManager.getComponent<KingdomComponent>(entity).aggressionLevel == KingdomComponent::AggressionLevel::Evil){
-                std::cout << "Entity " << entity << " is evil!" << std::endl;
-            }
+    static void establishKingdoms(ComponentManager* componentManager){
+        for (auto& entity : componentManager->getEntities<KingdomComponent>()){
+           std::cout << "Year " << START_YEAR << ": The ";
         }
     }
 };
